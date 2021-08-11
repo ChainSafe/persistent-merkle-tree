2e848a15490c2fbca5b27ee476fe32a0281b18ff
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-08-11 09:45:22 +0700

    Use digestTwoHashObject from as-sha256 0.2.3

--- src/node.ts
+++ src/node.ts
@@ -1,29 +1,57 @@
-import {hash} from "./hash";
+import {HashObject} from "@chainsafe/as-sha256";
+import {hashObjectToUint8Array, hashTwoObjects, uint8ArrayToHashObject} from "./hash";
 
 const ERR_INVALID_TREE = "Invalid tree";
 
-export abstract class Node {
+export abstract class Node implements HashObject {
+  h0 = 0;
+  h1 = 0;
+  h2 = 0;
+  h3 = 0;
+  h4 = 0;
+  h5 = 0;
+  h6 = 0;
+  h7 = 0;
+
   abstract root: Uint8Array;
+  abstract rootHashObject: HashObject;
   abstract left: Node;
   abstract right: Node;
+
+  applyHash(root: HashObject): void {
+    this.h0 = root.h0;
+    this.h1 = root.h1;
+    this.h2 = root.h2;
+    this.h3 = root.h3;
+    this.h4 = root.h4;
+    this.h5 = root.h5;
+    this.h6 = root.h6;
+    this.h7 = root.h7;
+  }
+
   abstract isLeaf(): boolean;
   abstract rebindLeft(left: Node): Node;
   abstract rebindRight(right: Node): Node;
 }
 
 export class BranchNode extends Node {
-  private _root: Uint8Array | null = null;
+  private hasRoot = false;
 
   constructor(private _left: Node, private _right: Node) {
     super();
     if (!_left || !_right) throw new Error(ERR_INVALID_TREE);
   }
 
-  get root(): Uint8Array {
-    if (!this._root) {
-      this._root = hash(this.left.root, this.right.root);
+  get rootHashObject(): HashObject {
+    if (!this.hasRoot) {
+      super.applyHash(hashTwoObjects(this.left.rootHashObject, this.right.rootHashObject));
+      this.hasRoot = true;
     }
-    return this._root as Uint8Array;
+    return this;
+  }
+
+  get root(): Uint8Array {
+    return hashObjectToUint8Array(this.rootHashObject);
   }
 
   isLeaf(): boolean {
@@ -48,13 +76,18 @@ export class BranchNode extends Node {
 }
 
 export class LeafNode extends Node {
-  constructor(private _root: Uint8Array) {
+  constructor(_root: Uint8Array) {
     super();
+    this.applyHash(uint8ArrayToHashObject(_root));
     if (_root.length !== 32) throw new Error(ERR_INVALID_TREE);
   }
 
+  get rootHashObject(): HashObject {
+    return this;
+  }
+
   get root(): Uint8Array {
-    return this._root;
+    return hashObjectToUint8Array(this);
   }
 
   isLeaf(): boolean {
